648d7b17057a7e2defa664b8115423560a78b9fb
nkoles <37773744+nkoles@users.noreply.github.com>
2024-10-05 16:08:25 +0100

    Simple Controls Added

---
+++ Assets/Scripts/Gameplay/CarScripts/CarController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    public float drivingForce;
+
+    public float turnSpeed;
+    public float maxRotation = 45;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        NonWheelApproach();
+    }
+
+    private void NonWheelApproach()
+    {
+        _rb.AddForce(drivingForce * transform.right * Input.GetAxisRaw("Horizontal"), ForceMode.Acceleration);
+
+        transform.rotation = Quaternion.Slerp(Quaternion.Euler(transform.eulerAngles), Quaternion.Euler(0, maxRotation * Input.GetAxisRaw("Horizontal"), 0), Time.fixedDeltaTime * turnSpeed * Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+    }
+}
