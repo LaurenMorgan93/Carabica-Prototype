419c9b367ded277cfbf62b9cb6dc245cf7e4c844
LaurenMorgan93 <130102298+LaurenMorgan93@users.noreply.github.com>
2024-10-05 18:43:47 +0100

    coffee pot work

--- Assets/Scripts/CoffeePot.cs
+++ Assets/Scripts/CoffeePot.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
 
 public class CoffeePot : MonoBehaviour  // this script is attached to the coffee pot object
 {
     public float coffeeLevel = 0; // Current coffee level 
     public float maxCoffeeLevel = 100f; // Maximum capacity of the coffee pot
+
+    public Slider coffeeMeter;
     
     private void OnParticleCollision(GameObject other)
     {
@@ -14,6 +19,8 @@ public class CoffeePot : MonoBehaviour  // this script is attached to the coffee
         }
     }
 
+ 
+
     private void FillCoffee()
     {
         // Increase coffee level 
@@ -21,6 +28,22 @@ public class CoffeePot : MonoBehaviour  // this script is attached to the coffee
         {
             coffeeLevel += 0.1f; // Adjust this to change how quickly the pot fills
             Debug.Log("Coffee Level: " + coffeeLevel);
+            coffeeMeter.value = coffeeLevel;
         }
+
+        if (coffeeLevel >= maxCoffeeLevel)
+        {
+            // drinking coffeee logic // reset the meter
+            EmptyCoffee();
+        }
+    }
+
+    public void EmptyCoffee()
+    {
+        // enter drinking logic here
+        
+        // empty the coffee pot and start again
+        coffeeLevel = 0f;
+        coffeeMeter.value = coffeeLevel;
     }
 }
