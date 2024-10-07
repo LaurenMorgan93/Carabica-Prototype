60ff69969dbd09e36aeba861c838829a313e26a5
simojian <simon.kondela@gmail.com>
2024-10-07 16:23:01 +0100

    added an awfully hot coffee pot

--- Assets/Scripts/ArmFollow.cs
+++ Assets/Scripts/ArmFollow.cs
@@ -11,7 +11,7 @@ public class ArmFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.position = ArmPivot.position;
     }
