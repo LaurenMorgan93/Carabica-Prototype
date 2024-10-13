861cdc5d88d4ac7468355e36a58437affee75bb9
nkoles <37773744+nkoles@users.noreply.github.com>
2024-10-13 20:57:46 +0100

    lil update yuppa

--- Assets/Scripts/Gameplay/PedestrianMovements/Pedestrian.cs
+++ Assets/Scripts/Gameplay/PedestrianMovements/Pedestrian.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Pedestrian : MonoBehaviour
 {
@@ -15,20 +16,41 @@ public class Pedestrian : MonoBehaviour
 
     public bool isInitialised = false;
 
+    private UnityEvent Reached = new UnityEvent();
+
+    private Coroutine movementRoutine;
+
     private void Awake()
     {
         _speed = Random.Range(randomRange.x, randomRange.y);
         _pieceMovementTrigger = Random.Range(3, 9);
+
+        Reached.AddListener(StartMovement);
     }
 
     private void Update()
     {
-        if(m_RoadPiece == RoadManager.instance.roadPieces[_pieceMovementTrigger] && isInitialised)
+        if(m_RoadPiece == RoadManager.instance.roadPieces[_pieceMovementTrigger] && isInitialised && movementRoutine == null)
+        {
+            Reached.Invoke();
+        }
+    }
+
+    public void StartMovement()
+    {
+        movementRoutine = StartCoroutine(Movement());
+    }
+
+    private IEnumerator Movement()
+    {
+        while (true)
         {
             if (m_Lane == 0)
                 transform.position += transform.right * _speed * Time.deltaTime;
             else
                 transform.position += transform.right * -_speed * Time.deltaTime;
+
+            yield return null;
         }
     }
 
